7b44cf2498f9d9fd4589873cf64b2f20ff974b5a
Tsutomu Yoshida <doguo@cisco.com>
2020-11-14 22:21:18 +0900

    added first view and passed functional test

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def home_page(request):
+    return HttpResponse('<html><title>To-Do Lists</title></html>')
 # Create your views here.
