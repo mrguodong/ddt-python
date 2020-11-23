fdfe2c000ff05ccb3c716742ed98f1ded363f4b4
Tsutomu Yoshida <doguo@cisco.com>
2020-11-23 20:23:36 +0900

    use env vars for pred seeting DEBUG

--- superlists/settings.py
+++ superlists/settings.py
@@ -22,12 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_o8cureqypkl43!o7(3@6(q4t9aaszjx=dd^y0y(j)1ne_&wz8'
+#SECRET_KEY = '_o8cureqypkl43!o7(3@6(q4t9aaszjx=dd^y0y(j)1ne_&wz8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-key'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
